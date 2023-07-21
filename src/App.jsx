5e38d9e99264ed5b64b9bfb35853a21cc4f9dd75
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-21 15:40:40 -0300

    instalo bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-     <h1>8</h1>
+      <h1>8</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
